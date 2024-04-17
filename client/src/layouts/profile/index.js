badafd083b3e65b8c0aeb7415d66ce59b86f21d9
chagna-yassine <yassinechagna01@gmail.com>
2024-04-17 17:58:27 +0100

    profile

--- client/src/layouts/profile/index.js
+++ client/src/layouts/profile/index.js
@@ -27,14 +27,7 @@ import PlatformSettings from "layouts/profile/components/PlatformSettings";
 import profilesListData from "layouts/profile/data/profilesListData";
 
 // Images
-import homeDecor1 from "assets/images/home-decor-1.jpg";
-import homeDecor2 from "assets/images/home-decor-2.jpg";
-import homeDecor3 from "assets/images/home-decor-3.jpg";
-import homeDecor4 from "assets/images/home-decor-4.jpeg";
-import team1 from "assets/images/team-1.jpg";
-import team2 from "assets/images/team-2.jpg";
-import team3 from "assets/images/team-3.jpg";
-import team4 from "assets/images/team-4.jpg";
+import Web from "assets/images/web.png";
 
 function Overview() {
   return (
@@ -44,10 +37,10 @@ function Overview() {
       <Header>
         <MDBox mt={5} mb={3}>
           <Grid container spacing={1}>
-            <Grid item xs={12} md={6} xl={4}>
+            {/* <Grid item xs={12} md={6} xl={4}>
               <PlatformSettings />
-            </Grid>
-            <Grid item xs={12} md={6} xl={4} sx={{ display: "flex" }}>
+            </Grid> */}
+            {/* <Grid item xs={12} md={6} xl={4} sx={{ display: "flex" }}>
               <Divider orientation="vertical" sx={{ ml: -2, mr: 1 }} />
               <ProfileInfoCard
                 title="profile information"
@@ -82,8 +75,8 @@ function Overview() {
             </Grid>
             <Grid item xs={12} xl={4}>
               <ProfilesList title="conversations" profiles={profilesListData} shadow={false} />
-            </Grid>
-          </Grid>
+            </Grid>*/}
+          </Grid> 
         </MDBox>
         <MDBox pt={2} px={2} lineHeight={1.25}>
           <MDTypography variant="h6" fontWeight="medium">
@@ -91,7 +84,7 @@ function Overview() {
           </MDTypography>
           <MDBox mb={1}>
             <MDTypography variant="button" color="text">
-              Architects design houses
+              Assignments
             </MDTypography>
           </MDBox>
         </MDBox>
@@ -99,8 +92,8 @@ function Overview() {
           <Grid container spacing={6}>
             <Grid item xs={12} md={6} xl={3}>
               <DefaultProjectCard
-                image={homeDecor1}
-                label="project #2"
+                image={Web}
+                label="project #1"
                 title="modern"
                 description="As Uber works through a huge amount of internal management turmoil."
                 action={{
@@ -109,72 +102,72 @@ function Overview() {
                   color: "info",
                   label: "view project",
                 }}
-                authors={[
-                  { image: team1, name: "Elena Morison" },
-                  { image: team2, name: "Ryan Milly" },
-                  { image: team3, name: "Nick Daniel" },
-                  { image: team4, name: "Peterson" },
-                ]}
+                // authors={[
+                //   { image: team1, name: "Elena Morison" },
+                //   { image: team2, name: "Ryan Milly" },
+                //   { image: team3, name: "Nick Daniel" },
+                //   { image: team4, name: "Peterson" },
+                // ]}
               />
             </Grid>
             <Grid item xs={12} md={6} xl={3}>
               <DefaultProjectCard
-                image={homeDecor2}
-                label="project #1"
+                image={Web}
+                label="project #2"
                 title="scandinavian"
-                description="Music is something that everyone has their own specific opinion about."
+                // description="Music is something that everyone has their own specific opinion about."
                 action={{
                   type: "internal",
                   route: "/pages/profile/profile-overview",
                   color: "info",
                   label: "view project",
                 }}
-                authors={[
-                  { image: team3, name: "Nick Daniel" },
-                  { image: team4, name: "Peterson" },
-                  { image: team1, name: "Elena Morison" },
-                  { image: team2, name: "Ryan Milly" },
-                ]}
+                // authors={[
+                //   { image: team3, name: "Nick Daniel" },
+                //   { image: team4, name: "Peterson" },
+                //   { image: team1, name: "Elena Morison" },
+                //   { image: team2, name: "Ryan Milly" },
+                // ]}
               />
             </Grid>
             <Grid item xs={12} md={6} xl={3}>
               <DefaultProjectCard
-                image={homeDecor3}
+                image={Web}
                 label="project #3"
                 title="minimalist"
-                description="Different people have different taste, and various types of music."
+                // description="Different people have different taste, and various types of music."
                 action={{
                   type: "internal",
                   route: "/pages/profile/profile-overview",
                   color: "info",
                   label: "view project",
                 }}
-                authors={[
-                  { image: team4, name: "Peterson" },
-                  { image: team3, name: "Nick Daniel" },
-                  { image: team2, name: "Ryan Milly" },
-                  { image: team1, name: "Elena Morison" },
-                ]}
+                // authors={[
+                //   { image: team4, name: "Peterson" },
+                //   { image: team3, name: "Nick Daniel" },
+                //   { image: team2, name: "Ryan Milly" },
+                //   { image: team1, name: "Elena Morison" },
+                // ]}
               />
             </Grid>
             <Grid item xs={12} md={6} xl={3}>
               <DefaultProjectCard
-                image={homeDecor4}
+                image={Web}
                 label="project #4"
                 title="gothic"
-                description="Why would anyone pick blue over pink? Pink is obviously a better color."
+                // description="Why would anyone pick blue over pink? Pink is obviously a better color."
                 action={{
                   type: "internal",
                   route: "/pages/profile/profile-overview",
                   color: "info",
                   label: "view project",
                 }}
-                authors={[
-                  { image: team4, name: "Peterson" },
-                  { image: team3, name: "Nick Daniel" },
-                  { image: team2, name: "Ryan Milly" },
-                  { image: team1, name: "Elena Morison" },
-                ]}
+                // authors={[
+                //   { image: team4, name: "Peterson" },
+                //   { image: team3, name: "Nick Daniel" },
+                //   { image: team2, name: "Ryan Milly" },
+                //   { image: team1, name: "Elena Morison" },
+                // ]}
               />
             </Grid>
           </Grid>
